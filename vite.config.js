8dc404f330333f0df043000a7220a90f4a4ebd3b
Meena-017 <161192726+Meena-017@users.noreply.github.com>
2024-10-19 08:20:37 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   base: "/frontend",
   plugins: [react()],
 })
+  export default {
+  base: '/subfolder-name/',
+};
+
